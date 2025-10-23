99632d15bd26a734fd19c57ea06d01e41e0320b7
SABER AMINE <saberamine000@gmail.com>
2025-10-23 11:57:01 +0200

    Initial commit

---
+++ src/main/java/com/dev/plateforme_de_dons/PlateformeDeDonsApplication.java
@@ -0,0 +1,13 @@
+package com.dev.plateforme_de_dons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateformeDeDonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateformeDeDonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/plateforme_de_dons/PlateformeDeDonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.plateforme_de_dons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlateformeDeDonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
